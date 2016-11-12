8525b17dd482ca79b67125596e2e6c663c26e3ce
Wojtek Swiderski <wswiders@uwaterloo.ca>
2016-11-12 15:07:57 -0800

    Background subtraction

--- vision/src/HumanDetector.cpp
+++ vision/src/HumanDetector.cpp
@@ -4,6 +4,8 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv2/video/background_segm.hpp>
+#include <opencv2/bgsegm.hpp>
 
 using namespace cv;
 
@@ -55,12 +57,69 @@ public:
   }
 };
 
+#define RESIZE_FACTOR 2
+
+RNG rng(12345);
+
+class BackgroundMask {
+public:
+  static bool mask(std::string video_file) {
+    Mat frame;
+    Mat resizeF;
+    Mat invF;
+    Mat drawing;
+    Mat fgMaskMOG;
+    Mat fgMaskMOG2;
+
+    Ptr<BackgroundSubtractor> pMOG = bgsegm::createBackgroundSubtractorMOG();
+    VideoCapture video(video_file);
+
+    if(!video.isOpened()) {
+      return false;
+    }
+
+    namedWindow("Masked", 1);
+    while(1) {
+      if (!video.read(frame)) {
+        break;
+      }
+
+      std::vector<Vec4i> hierarchy;
+      std::vector<std::vector<Point> > contours;
+      resize(frame, resizeF, Size(frame.size().width / RESIZE_FACTOR,
+          frame.size().height / RESIZE_FACTOR));
+
+      pMOG->apply(resizeF, fgMaskMOG);
+      bitwise_not(fgMaskMOG, invF);
+
+      Mat kernel = getStructuringElement(MORPH_ELLIPSE, Point(3, 3));
+      morphologyEx(invF, drawing, MORPH_OPEN, kernel);
+
+      // findContours(invF, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+      // drawing = Mat::zeros( invF.size(), CV_8UC3 );
+      // for (int i = 0; i < contours.size(); ++i) {
+      //   drawContours( drawing, contours, i, 255, 4);
+      // }
+
+      // imshow("Origin", resizeF);
+      imshow("MOG", drawing);
+
+      if (waitKey(1) >= 0) {
+        break;
+      }
+    }
+
+    return true;
+  }
+};
+
 int main(int argc, char** argv) {
   if (argc != 2) {
     printf("Usage: %s <detection_video>", argv[0]);
     return -1;
   }
 
-  HumanTracker::trackVideo(argv[1]);
+  BackgroundMask::mask(argv[1]);
   return 0;
 }
